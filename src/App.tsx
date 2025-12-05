a1582b32cebde8b820c079607cac8c3f3d4b5bf8
Saurabh Sangewar <saurabh.sangewar@amla.io>
2025-12-05 06:53:27 +0530

    Create Header Component with initial React Router navigation setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
+import HomePage from './Pages/HomePage'
+import ProductListPage from './Pages/ProductListPage'
+import { Route, Routes } from 'react-router'
+import NotFound from './Pages/NotFound'
+import Header from './Components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+const [cartCount, setCartCount] = useState<number>(5);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <Header cartCount={cartCount} />
+      <main style={{ padding: 20 }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductListPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
     </>
   )
 }
---
+++ src/Components/Header.tsx
@@ -0,0 +1,89 @@
+import { FiShoppingCart } from "react-icons/fi";
+import { NavLink } from "react-router";
+
+interface HeaderProps {
+  cartCount?: number;
+}
+
+const Header = ({ cartCount }: HeaderProps) => {
+  const linkStyle = {
+    textDecoration: "none",
+    color: "#333",
+  };
+
+  const activeStyle = {
+    fontWeight: "700",
+    color: "#03fa62ff",
+  };
+
+  return (
+    <header
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: "15px 30px",
+        background: "#fff",
+        borderBottom: "1px solid #eee",
+        position: "sticky",
+        top: 0,
+        zIndex: 100,
+      }}
+    >
+      {/*Logo */}
+      <NavLink to="/" style={linkStyle}>
+        MyShop
+      </NavLink>
+
+      {/*Nav Menu */}
+      <nav
+        style={{
+          display: "flex",
+          gap: "25px",
+          fontSize: "16px",
+        }}
+      >
+        <NavLink
+          to="/"
+          style={({ isActive }) => (isActive ? activeStyle : linkStyle)}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/products"
+          style={({ isActive }) => (isActive ? activeStyle : linkStyle)}
+        >
+          Products
+        </NavLink>
+        <NavLink
+          to="/contact"
+          style={({ isActive }) => (isActive ? activeStyle : linkStyle)}
+        >
+          Contact
+        </NavLink>
+      </nav>
+
+      {/*Cart */}
+      <div style={{ position: "relative", cursor: "pointer" }}>
+        <FiShoppingCart size={26} />
+
+        <span
+          style={{
+            position: "absolute",
+            top: "-6px",
+            right: "-10px",
+            background: "red",
+            color: "white",
+            borderRadius: "50%",
+            fontSize: "12px",
+            padding: "2px 6px",
+          }}
+        >
+          {cartCount}
+        </span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
